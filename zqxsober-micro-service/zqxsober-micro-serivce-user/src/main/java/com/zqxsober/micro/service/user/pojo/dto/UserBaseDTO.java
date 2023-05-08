2ec8dcac9adfb0a03d233f51016dcb6bb1530fbc
zqxsober <zqxsober@163.com>
2023-05-08 16:37:36 +0800

    edit:add user dto

---
+++ zqxsober-micro-service/zqxsober-micro-serivce-user/src/main/java/com/zqxsober/micro/service/user/pojo/dto/UserBaseDTO.java
@@ -0,0 +1,22 @@
+package com.zqxsober.micro.service.user.pojo.dto;
+
+import lombok.Data;
+
+/**
+ * @Author: zqxsober
+ * @Description: UserBaseDTO 类
+ * @Date: 2023-05-08 16:36
+ */
+@Data
+public class UserBaseDTO {
+
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 用户姓名
+     */
+    private String userName;
+}
