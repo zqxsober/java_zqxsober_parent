81eb8bcd891a65c89d91563379ff51f6bae9451f
zqxsober <zqxsober@163.com>
2023-07-17 16:50:38 +0800

    edit:add logs

--- zqxsober-application/zqxsober-web/src/main/java/com/zqxosber/web/module/user/controller/UserController.java
+++ zqxsober-application/zqxsober-web/src/main/java/com/zqxosber/web/module/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.zqxsober.api.user.pojo.bo.UserInfoBo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: UserController 类
  * @Date: 2022-07-19 09:43
  */
+@Slf4j
 @Api(tags = "会员接口")
 @RestController
 @RequestMapping("/user")
@@ -26,7 +28,8 @@ public class UserController extends BaseController {
 
     @GetMapping("/getUser")
     @ApiOperation(value = "获取会员信息", response = String.class)
-    public ResponseEntity<?> getUser(@RequestParam(required = false) String userId){
+    public ResponseEntity<?> getUser(@RequestParam(required = false) String userId) {
+        log.info("获取会员信息，请求参数：【{}】", userId);
         return ResponseEntity.ok(super.getUserInfo(userId));
     }
 
