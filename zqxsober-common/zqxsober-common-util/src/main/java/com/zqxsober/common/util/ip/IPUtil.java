d8cc2a2af901c14384153634197cc5418d512b52
zqxsober <zqxsober@163.com>
2024-06-21 11:34:06 +0800

    add【develop】add utils

---
+++ zqxsober-common/zqxsober-common-util/src/main/java/com/zqxsober/common/util/ip/IPUtil.java
@@ -0,0 +1,50 @@
+package com.zqxsober.common.util.ip;
+
+import com.alibaba.excel.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: zqxsober
+ * @Description: IPUtil 类
+ * @Date: 2024-06-03 13:40
+ */
+public class IPUtil {
+
+    public static String getRemoteAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        if (StringUtils.isNotBlank(ip) && ip.indexOf(",") != -1) {
+            ip = ip.split(",")[0];
+        }
+
+        if (StringUtils.isNotBlank(ip) && ip.length() > 20) {
+            ip = ip.substring(0, 20);
+        }
+
+        return ip;
+    }
+}
