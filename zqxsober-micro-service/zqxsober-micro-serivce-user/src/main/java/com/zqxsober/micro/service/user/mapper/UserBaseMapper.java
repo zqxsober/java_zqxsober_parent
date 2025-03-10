2d9d081679c68d9588195a38d7ae5b9641c3fe04
zqxsober <zqxsober@163.com>
2025-03-10 09:13:07 +0800

    add【develop】add get user info

--- zqxsober-micro-service/zqxsober-micro-serivce-user/src/main/java/com/zqxsober/micro/service/user/mapper/UserBaseMapper.java
+++ zqxsober-micro-service/zqxsober-micro-serivce-user/src/main/java/com/zqxsober/micro/service/user/mapper/UserBaseMapper.java
@@ -2,6 +2,7 @@ package com.zqxsober.micro.service.user.mapper;
 
 import com.zqxsober.micro.service.user.pojo.po.UserBase;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author zqxsober
  * @since 2022-07-19
  */
+@Mapper
 public interface UserBaseMapper extends BaseMapper<UserBase> {
 
 }
--- zqxsober-micro-service/zqxsober-micro-serivce-user/src/main/java/com/zqxsober/micro/service/user/service/impl/UserBaseServiceImpl.java
+++ zqxsober-micro-service/zqxsober-micro-serivce-user/src/main/java/com/zqxsober/micro/service/user/service/impl/UserBaseServiceImpl.java
@@ -4,6 +4,7 @@ import com.zqxsober.micro.service.user.pojo.po.UserBase;
 import com.zqxsober.micro.service.user.mapper.UserBaseMapper;
 import com.zqxsober.micro.service.user.service.UserBaseService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +16,13 @@ import org.springframework.stereotype.Service;
  * @since 2022-07-19
  */
 @Service
+@RequiredArgsConstructor
 public class UserBaseServiceImpl extends ServiceImpl<UserBaseMapper, UserBase> implements UserBaseService {
 
+    private final UserBaseMapper userBaseMapper;
+
+    @Override
+    public UserBase getUserBase(String id) {
+        return this.getById(id);
+    }
 }
