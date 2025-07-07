1b45e9ff098ce705163711c3d31be69da0ebdd4c
zqxsober <zqxsober@163.com>
2025-07-07 11:00:56 +0800

    add rule microservice

---
+++ zqxsober-micro-service/zqxsober-micro-service-rule/src/main/java/com/zqxsober/MircServiceRuleApplication.java
@@ -0,0 +1,16 @@
+package com.zqxsober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zqxsober
+ * @Description: MircServiceRuleApplication 类
+ * @Date: 2022-01-04 16:26
+ */
+@SpringBootApplication
+public class MircServiceRuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MircServiceRuleApplication.class);
+    }
+}
